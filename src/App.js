9a2c528472600bd80a113c7d0338d9378410d004
antnf3 <antnf3@gmail.com>
2019-03-08 07:34:36 +0900

    babel-polyfill 설정 완료

--- src/App.js
+++ src/App.js
@@ -1,4 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styles from './styles.scss';
 
-export default () => <div className={styles.title}>테스aaa트입니다</div>;
+class App extends Component {
+	state = {};
+
+	componentDidMount() {
+		this._getMovies();
+	}
+
+	_getMovies = async () => {
+		const movies = await this._callAPI();
+		this.setState({
+			movies,
+		});
+		console.log(movies);
+	};
+
+	_callAPI = () => {
+		return fetch('https://yts.am/api/v2/list_movies.json?sort_by=download_count')
+			.then(res => res.json())
+			.then(json => {
+				return json.data.movies;
+			})
+			.catch(err => console.log(err));
+	};
+
+	render() {
+		const { movies } = this.state;
+		return <div>aaaaa</div>;
+	}
+}
+
+export default App;
